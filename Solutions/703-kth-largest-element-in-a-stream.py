e5288b1c8ffe0fb9cba5fa302b01b609a34ad514
Wojtek <pulchnymamut@gmail.com>
2023-08-21 20:29:19 +0200

    Update 703-kth-largest-element-in-a-stream.py

--- Solutions/703-kth-largest-element-in-a-stream.py
+++ Solutions/703-kth-largest-element-in-a-stream.py
@@ -2,14 +2,23 @@ import heapq
 from typing import List
 
 class KthLargest:
+
     def __init__(self, k: int, nums: List[int]):
+        # Initialize the class with the given 'k' value and an empty min-heap.
         self.k = k
         self.min_heap = []
+
+        # Initialize the min-heap with the first 'k' elements from 'nums'.
         for num in nums:
             self.add(num)
 
     def add(self, val: int) -> int:
+        # Add the new value 'val' to the min-heap.
         heapq.heappush(self.min_heap, val)
+
+        # If the size of the min-heap exceeds 'k', remove the smallest element.
         if len(self.min_heap) > self.k:
             heapq.heappop(self.min_heap)
+
+        # The Kth largest element is always at the top of the min-heap.
         return self.min_heap[0]
