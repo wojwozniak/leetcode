06f58f8a759b77d61a0ea399d90aa1d57f149ea4
Wojtek <pulchnymamut@gmail.com>
2025-06-21 11:51:20 +0200

    Create sort_with_correnspondence.rs

---
+++ utils/sort_with_correnspondence.rs
@@ -0,0 +1,10 @@
+fn sort_with_correspondence<T: Ord, U>(a: &mut [T], b: &mut [U]) {
+    let mut zipped: Vec<_> = a.iter().cloned().zip(b.iter().cloned()).collect();
+
+    zipped.sort_by(|x, y| x.0.cmp(&y.0));
+
+    for (i, (a_val, b_val)) in zipped.into_iter().enumerate() {
+        a[i] = a_val;
+        b[i] = b_val;
+    }
+}
