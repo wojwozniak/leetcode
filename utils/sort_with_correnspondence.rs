accdc4a322b6982a266bf4f3cdeb646320a07cee
Wojtek <pulchnymamut@gmail.com>
2025-06-21 11:53:39 +0200

    Update sort_with_correnspondence.rs

--- utils/sort_with_correnspondence.rs
+++ utils/sort_with_correnspondence.rs
@@ -1,3 +1,7 @@
+/**
+ * Sort array a
+ * Reorder array b so respective indexes stay the same
+ */
 fn sort_with_correspondence<T: Ord, U>(a: &mut [T], b: &mut [U]) {
     let mut zipped: Vec<_> = a.iter().cloned().zip(b.iter().cloned()).collect();
 
